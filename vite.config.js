8069c0d7f2e4097573e8c3c363ff653100d83d7a
Rhinoffensive <renantaylan@gmail.com>
2025-04-05 17:43:25 +1300

    Disabled allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/maas-karsilastirici/',
   server: {
     host: true, // dış dünyaya açılmasını sağlar
-    allowedHosts: [
-      '87fc-2404-4408-67a1-e600-8da-859d-4c97-7f04.ngrok-free.app' // güncel ngrok adresin
-    ]
+    // allowedHosts: [
+    //   '87fc-2404-4408-67a1-e600-8da-859d-4c97-7f04.ngrok-free.app' // güncel ngrok adresin
+    // ]
+
+
   }
 });
